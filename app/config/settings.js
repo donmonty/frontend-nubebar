9594048dfb318dabe9ae4b5d57800d3d4fc1b352
Montero <montero@Montero.local>
2022-11-08 21:00:19 -0600

    Fixes for SDK 45 migration

--- app/config/settings.js
+++ app/config/settings.js
@@ -17,7 +17,7 @@ const settings = {
 
 const getCurrentSettings = () => {
   if (__DEV__) return settings.dev;
-  if (Constants.manifest.releaseChannel === "staging") return settings.staging;
+  // if (Constants.manifest.releaseChannel === "staging") return settings.staging;
   return settings.prod;
 };
 
