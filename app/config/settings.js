d75ae0089d8b3ffc21171ac0c1cf8daf8d75b0b8
Montero <montero@Montero.local>
2023-02-23 16:58:17 -0600

    Edited api dev URL

--- app/config/settings.js
+++ app/config/settings.js
@@ -2,7 +2,8 @@ import Constants from "expo-constants";
 
 const settings = {
   dev: {
-    apiUrl: "http://192.168.1.71:8000",
+    apiUrl: "https://api-nubebar-django-rmwos.ondigitalocean.app",
+    // apiUrl: "http://192.168.1.71:8000",
     // apiUrl: "https://945e11bf-c5e6-492a-a37f-5c77ad915de1.mock.pstmn.io",
     tokenType: 'Token'
   },
