e42d3a5f9a440fe135e7c3c8b5a58a6a70bdb8bd
Montero <antonio.montero@gmail.com>
2024-11-14 19:15:47 -0600

    Remove expo-app-loading, update Expo

--- App.js
+++ App.js
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
 import { NavigationContainer } from "@react-navigation/native";
-import { AppLoading } from "expo-app-loading";
 
 import navigationTheme from "./app/navigation/navigationTheme";
 import AppNavigator from "./app/navigation/AppNavigator";
